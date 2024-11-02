5c55cbc33708bdcbb11f59d457f69d2948c7aff4
jinghengchen <jic144@pitt.edu>
2024-11-01 21:09:43 -0700

    deleted readme on this branch

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 # you need to install shiny, shinywidgets and ipyleaflet to be able to run this
 from shiny import App, ui, reactive, render
-from ipyleaflet import Map
+from ipyleaflet import Map, GeoJSON
 from shinywidgets import output_widget, render_widget
 
 
 # data needed to flow into the app
 ten_metro = ['Seattle', 'New York', 'Boston', 'Chicago', "Washington DC", "Los Angeles", "San Francisco", "Phoenix", "Houston", "Jacksonville"]
+
+coordinates = {
+    'Seattle': (47.608013, -122.335167),
+    'New York': (40.712776, -74.005974), 
+    'Boston': (42.360081, -71.058884), 
+    'Chicago': (41.878113, -87.629799), 
+    "Washington DC": (38.907192, -77.036873),
+    "Los Angeles": (34.073520, -118.253879), 
+    "San Francisco": (0,0),
+    "Phoenix": (0,0), 
+    "Houston": (0,0), 
+    "Jacksonville": (0,0)
+    }
 
 # input dataframes needed here
 
@@ -22,6 +35,12 @@ app_ui = ui.page_fluid(
         choices = ten_metro
     ),  
     
+    ui.input_select(
+        id = "census_tract",
+        label = "Select a region:",
+        choices = ['census tract 1', 'census tract 2']
+    ),
+
     # this creates a container to display some text that will be defined in the server function
     ui.output_text(
         id = "selected_metro"
@@ -39,6 +58,6 @@ def server(input, output, session):
 
     @render_widget  
     def map():
-        return Map(center=(50.6252978589571, 0.34580993652344), zoom=3)  
+        return Map(center= coordinates[input.metro_area()], zoom=10)  
 
 app = App(app_ui, server)
