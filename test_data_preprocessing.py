cfe68aab69b30df324b28eac7c63fca736b3f711
jinghengchen <jic144@pitt.edu>
2024-11-22 19:16:19 -0800

    "Git told me I need to commit before I can pull. "

---
+++ test_data_preprocessing.py
@@ -0,0 +1,10 @@
+import data_preprocessing
+
+data = pd.read_csv("CleanedMetropolitanCensusTractsData.csv")
+
+get_city_ind_avg('Seattle', data)
+
+get_city_life_exp('Seattle', data)
+get_city_life_exp('Jacksonville', data)
+
+get_city_life_exp_dist('Seattle', data)
